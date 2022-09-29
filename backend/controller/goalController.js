ef13c674655281ca613c40b7e6b1d906153aeb0e
Admanii <ahmedriaz-50@hotmail.com>
2022-09-30 02:36:05 +0500

    Rest API Goals

--- backend/controller/goalController.js
+++ backend/controller/goalController.js
@@ -1,9 +1,11 @@
 const asyncHandler = require('express-async-handler')
-
+const Goal = require('../models/goalModel')
 // get goals
 // route:   GET /api/goals
 const getGoals = asyncHandler(async (req, res) => {
-    res.status(200).json({message: 'Get goals'})
+    const goals = await Goal.find()
+
+    res.status(200).json(goals)
 })
 
 // post goals
@@ -13,19 +15,47 @@ const postGoals = asyncHandler(async (req, res) => {
         res.status(400)
         throw new Error('Please add a Text Field')
     }
-    res.status(200).json({message: 'Set goals'})
+
+    const goal = await Goal.create({
+        text: req.body.text
+    })
+    res.status(200).json(goal)
 })
 
 // Update goals
 // route:   GET /api/goals
 const updateGoals = asyncHandler(async (req, res) => {
-    res.status(200).json({message: 'Update goal ' + req.params.id})
+    const goal = await Goal.findById(req.params.id)
+
+    if(!goal){
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+    const updateGoal = await Goal.findByIdAndUpdate(
+        req.params.id, 
+        req.body, 
+        {new: true,}
+        )
+
+    
+    res.status(200).json(updateGoal)
 })
 
 // delete goals
 // route:   GET /api/goals
 const deleteGoals = asyncHandler(async (req, res) => {
-    res.status(200).json({message: 'Delete goal ' + req.params.id})
+    const goal = await Goal.findById(req.params.id)
+    if(!goal){
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+    // const deleteGoal = await Goal.findByIdAndDelete(
+    //     req.params.id, 
+    //     )
+    await goal.remove()
+
+
+    res.status(200).json({id: req.params.id})
 })
 
 module.exports = {
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,17 @@
 const express = require('express')
+const colors = require('colors')
 const dotenv = require('dotenv').config()
 const {errorHandler} = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
+const mongoose = require('mongoose')
 
 const app = express()
+
+mongoose.connect("mongodb+srv://admani:283790@cluster0.psomssl.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true }, (e) =>
+  console.log('MongoDB Connected')
+);
+//connectDB()
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
